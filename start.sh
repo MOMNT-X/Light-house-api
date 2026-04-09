da93e84f6468a7beec06374519b8d9567dbb9997
Moment <adeogopelumi05@gmail.com>
2026-04-09 13:25:11 +0100

    it is well

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "🗃️  Running Prisma db push..."
-npx prisma db push
+npx prisma db push --url "$DIRECT_URL"
 
 echo "🌱  Running seed..."
 npx ts-node -r tsconfig-paths/register prisma/seed.ts || echo "⚠️  Seed skipped (already seeded or error)."
